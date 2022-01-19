50b10d7b9332c01ee31528ee7b0aabf236adc12d
Robin Karlsson <robink90@gmail.com>
2022-01-19 14:37:05 +0100

    v1

---
+++ BookAPI/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace BookAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Description { get; set; }
+    }
+}
---
+++ BookAPI/Models/BookContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookAPI.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
